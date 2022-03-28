e87bc37df9e6d97523b7072a49939531e9e90f8e
JacobFYeomans <90638405+JacobFYeomans@users.noreply.github.com>
2022-03-28 15:25:19 -0300

    added comments

--- GlobalSettings.cs
+++ GlobalSettings.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Text_Based_RPG
 {
-    class GlobalSettings //list of things that are hardcoded
+    class GlobalSettings //list of things that are hardcoded/settings that can be changed easily here
     {
         public string[] mapRawData = System.IO.File.ReadAllLines("map.txt");
     }
